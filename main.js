e7a5c8064f8c7a68a6339901df51be745b48726e
Jeff Newcomer <jrnewk@gmail.com>
2018-06-25 15:28:16 -0700

    Defined a function that takes an item from the catalog item and returns a bootstrap card component DOM tree containing the brand, name, and price of the item. #1

---
+++ main.js
@@ -0,0 +1,123 @@
+/* eslint-disable no-unused-vars */
+
+var app = {
+  view: 'catalog',
+  catalog: {
+    items: [
+      {
+        itemId: 1,
+        brand: 'Ludwig',
+        name: 'Black Beauty',
+        price: 749.99,
+        description: 'A snare drum for people with too many drumsticks.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'United States',
+        imageUrl: 'https://goo.gl/ubY0QA'
+      },
+      {
+        itemId: 2,
+        name: 'Darbuka',
+        brand: 'Vatan',
+        price: 79.99,
+        description: 'A hand drum for people who like belly dancing.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Turkey',
+        imageUrl: 'https://goo.gl/Q7X6VB'
+      },
+      {
+        itemId: 3,
+        name: 'Djembe',
+        brand: 'Remo',
+        price: 49.99,
+        description: 'A hand drum for people with dreadlocks.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'West Africa',
+        imageUrl: 'https://goo.gl/iKtU8x'
+      },
+      {
+        itemId: 4,
+        name: 'Hi-Hats',
+        brand: 'Paiste',
+        price: 400,
+        description: 'A set of hats for people who spare no expense.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Switzerland',
+        imageUrl: 'https://goo.gl/LV9l8O'
+      },
+      {
+        itemId: 5,
+        name: 'Giant Step',
+        brand: 'Sonor',
+        price: 599,
+        description: 'A pedal for metal drummers with only one foot.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Germany',
+        imageUrl: 'https://goo.gl/zsFo1q'
+      },
+      {
+        itemId: 6,
+        name: 'Travis Barker Signature',
+        brand: 'Zildjian',
+        price: 24.99,
+        description: 'A set of drumsticks from the 90s.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'United States',
+        imageUrl: 'https://goo.gl/Cro3tL'
+      },
+      {
+        itemId: 7,
+        name: 'Bodhran',
+        brand: 'Meinl',
+        price: 129.99,
+        description: 'A frame drum for people who like to jig.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Ireland',
+        imageUrl: 'https://goo.gl/YNb74O'
+      },
+      {
+        itemId: 8,
+        name: 'TR-09',
+        brand: 'Roland',
+        price: 399,
+        description: 'A drum machine for people with no desk space.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Japan',
+        imageUrl: 'https://goo.gl/gJYUxz'
+      }
+    ]
+  },
+  details: {
+    item: null
+  }
+}
+
+function createCard(card) {
+  var $card = document.createElement('div')
+  $card.classList.add('card')
+
+  var $image = document.createElement('img')
+  $card.appendChild($image)
+  $image.classList.add('card-img-top')
+  $image.setAttribute('src', app.catalog.items[0].imageUrl)
+
+  var $cardBody = document.createElement('div')
+  $card.appendChild($cardBody)
+  $cardBody.classList.add('card-body')
+
+  var $brand = document.createElement('h5')
+  $cardBody.appendChild($brand)
+  $brand.classList.add('card-title')
+  $brand.textContent = app.catalog.items[0].brand
+
+  var $name = document.createElement('h5')
+  $cardBody.appendChild($name)
+  $name.classList.add('card-title')
+  $name.textContent = app.catalog.items[0].name
+
+  var $price = document.createElement('h5')
+  $cardBody.appendChild($price)
+  $price.classList.add('card-title')
+  $price.textContent = app.catalog.items[0].price
+
+  return $card
+}
